9ecb578e094ce5093ced0fb9d37091ae654b45fd
TheCodingWorkshop <riccodingworkshop@gmail.com>
2020-12-02 18:27:54 +0000

    added paginator utility class

--- index.php
+++ index.php
@@ -4,6 +4,17 @@ $autolaod = ROOT_PATH . '/vendor/autoload.php';
 if (is_file($autolaod)) {
     require $autolaod;
 }
+
+/**
+ * PHP 5.4 ships with a built in web server for development. This server
+ * allows us to run silex without any configuration. However in order
+ * to server static files we need to handle it nicely
+ */
+$filename = __DIR__ . preg_replace( '#(\?.*)$#', '', $_SERVER['REQUEST_URI'] );
+if ( php_sapi_name() == 'cli-server' && is_file( $filename ) ) {
+    return false;
+}
+
 use Magma\Application\Application;
 $app = new Application(ROOT_PATH);
 $app->run()
---
+++ src/Magma/Utility/Paginator.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Magma\Utility;
+
+class Paginator
+{
+    /** @var float */
+    protected float $totalPages;
+
+    /** @var float */
+    protected float $offset;
+
+    /** @var int */
+    protected int $page;
+
+    /**
+     * Class constructor
+     *
+     * @param integer $totalRecords Total number of records
+     * @param integer $recordsPerPage Number of records on each page
+     * @param string $page Current page
+     *
+     * @return void
+     */
+    public function __construct(int $totalRecords, int $recordsPerPage, int $page)
+    {
+        // Make sure the page number is within a valid range from 1 to the total number of pages
+        $this->totalPages = ceil($totalRecords / $recordsPerPage);
+        $data = [
+            'options' => [
+                'default' => 1,
+                'min_range' => 1,
+                'max_range' => $this->totalPages
+                ]
+            ];
+        $this->page = filter_var($page, FILTER_VALIDATE_INT, $data);
+        // Calculate the starting record based on the page and number of records per page
+        $this->offset = $totalRecords * ($this->page - 1);
+    }
+
+    /**
+     * Get the starting record within the SQL Query
+     * 
+     * @return int
+     */
+    public function getOffset()
+    {
+        return (int)$this->offset;
+    }
+
+    /**
+     * Get the current page
+     * 
+     * @return void
+     */
+    public function getPage()
+    {
+        return (int)$this->page;
+    }
+
+    /**
+     * Get the total number of pages
+     * 
+     * @return int
+     */
+    public function getTotalPages()
+    {
+        return (int)$this->totalPages;
+    }
+
+
+}
